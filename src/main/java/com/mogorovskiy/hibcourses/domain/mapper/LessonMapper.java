3cb522650dfd200e7edeac0b6ea42a552e72027b
mogorovskiy <mgrvsky2@gmail.com>
2026-02-18 13:44:32 +0200

    clean-code

--- src/main/java/com/mogorovskiy/hibcourses/domain/mapper/LessonMapper.java
+++ src/main/java/com/mogorovskiy/hibcourses/domain/mapper/LessonMapper.java
@@ -3,7 +3,6 @@ package com.mogorovskiy.hibcourses.domain.mapper;
 import com.mogorovskiy.hibcourses.domain.dto.LessonDto;
 import com.mogorovskiy.hibcourses.domain.entities.LessonEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LessonMapper {
